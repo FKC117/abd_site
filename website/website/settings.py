d2937717e5817f8cfd4110bb99966e9d1f371103
FKC 117 <fazlul.karim117@gmail.com>
2026-04-19 18:11:58 +0600

    first commit

--- website/website/settings.py
+++ website/website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+3snl6=(bme*_=y3mnp(g^*nf*%=$uko#(7=6vm^=%g=u!c4f$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["34.9.124.100", "analyticabd.xyz"]
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Dhaka"
 
 USE_I18N = True
 
